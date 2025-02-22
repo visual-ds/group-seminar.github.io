40b24cbd17b0b810a7c5415dcab44e99c79fa64b
Sante <lsantet@unsa.edu.pe>
2025-02-22 17:47:19 -0300

    fix: 🐛 deploy

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',  // Importante para generar archivos estáticos
-  basePath: 'https://github.com/visual-ds/seminar-visualds/', // Reemplaza con el nombre de tu repositorio
   images: {
     unoptimized: true, // Necesario para exportación estática
   }
