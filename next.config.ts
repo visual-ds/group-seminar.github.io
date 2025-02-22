a743cfdd89a14b25fce086fd0c644bfe6138aefc
Sante <lsantet@unsa.edu.pe>
2025-02-22 17:56:55 -0300

    fix: 🐛 deploy

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,8 @@ const nextConfig: NextConfig = {
   output: 'export',  // Importante para generar archivos estáticos
   images: {
     unoptimized: true, // Necesario para exportación estática
-  }
+  },
+  swcMinify: false,
 };
 
 export default nextConfig;
